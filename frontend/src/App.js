f289f361cb2d0ddf7784f7b7652cb0581ee390a2
Priyank Satasiya <35215118+priyank81099@users.noreply.github.com>
2020-07-17 16:34:12 +0530

    Add files via upload

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component }  from "react";
 import { Switch, Route, BrowserRouter as Router } from "react-router-dom";
 import HomePageLayout from "./components/Homepage/index.js";
 import Profile from "./components/user_profile/Profile";
@@ -10,24 +10,51 @@ import Help from "./components/Footerpages/Help"
 import ForgotPassword from "./components/Auth/ForgotPassword.js";
 import ResetPassword from "./components/Auth/ResetPassword.js";
 import './index.css';
+import axios from "axios";
+import { SERVER_URL } from "./utils/constants";
 
-const App = () => {
-  return (
-    <div>
-      <Router>
-        <Switch>
-          <Route path="/" exact component={HomePageLayout} />
-          <Route path="/about" component={About_us} />
-          <Route path="/help" component={Help}/>
-          <Route path="/profile" component={Profile} />
-          <Route path="/login" component={Login} />
-          <Route path="/signup" component={SignUpForm} />
-          <Route path="/front" component={Front} />
-          <Route path="/forgotpassword" exact component={ForgotPassword} />
-          <Route path="/resetpassword/:slug" component={ResetPassword} />
-        </Switch>
-      </Router>
-    </div>
-  );
+class App extends Component {
+
+  state = {
+    users: [],
+    filteredList: []
+  }
+
+  componentDidMount() {
+    axios.get(`${SERVER_URL}/users`).then(users => {
+      const { data } = users;
+      this.setState({
+        users: data,
+        filteredList: data
+      });
+    });
+  }
+
+  render(){
+    const { users } = this.state;
+    return (
+      <div>
+        {
+          users.map(user =>{
+            localStorage.setItem(user._id+user.first_name+" "+user.last_name, user.profile_image)
+          })
+        }
+        <Router>
+          <Switch>
+            <Route path="/" exact component={HomePageLayout} />
+            <Route path="/about" component={About_us} />
+            <Route path="/help" component={Help}/>	
+            <Route path="/profile" component={Profile} />
+            <Route path="/login" component={Login} />
+            <Route path="/signup" component={SignUpForm} />
+            <Route path="/front" component={Front} />
+            <Route path="/forgotpassword" exact component={ForgotPassword} />
+            <Route path="/resetpassword/:slug" component={ResetPassword} />
+          </Switch>
+        </Router>
+      </div>
+    );
+  }
 };
+
 export default App;
